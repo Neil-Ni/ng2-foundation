fde03e24787c1e109aaf1087869561d520b161c8
Neil-Ni <tzuyangni@gmail.com>
2016-06-15 02:40:27 -0400

    add rating

--- ng2-foundation.ts
+++ ng2-foundation.ts
@@ -2,18 +2,21 @@ import { AlertComponent }         from './components/alert';
 import { BUTTON_DIRECTIVES }      from './components/buttons';
 import { PAGINATION_DIRECTIVES }  from './components/pagination';
 import { PROGRESSBAR_DIRECTIVES } from './components/progressbar';
+import { RatingComponent }        from './components/rating';
 
 export * from './components/alert';
 export * from './components/buttons';
 export * from './components/pagination';
 export * from './components/progressbar';
+export * from './components/rating';
 
 export default {
   directives: [
     AlertComponent,
     BUTTON_DIRECTIVES,
     PAGINATION_DIRECTIVES,
-    PROGRESSBAR_DIRECTIVES
+    PROGRESSBAR_DIRECTIVES,
+    RatingComponent
   ],
   providers: []
 };
