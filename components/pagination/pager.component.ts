27416a085040366b83f6a1a31faf6477179132e7
Neil-Ni <tzuyangni@gmail.com>
2016-06-14 21:01:15 -0400

    add pagination

---
+++ components/pagination/pager.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit, ElementRef, Renderer, Self } from '@angular/core';
+import { NgModel, NgClass } from '@angular/common';
+import { PaginationComponent } from './pagination.component';
+
+const pagerConfig = {
+  itemsPerPage: 10,
+  previousText: '« Previous',
+  nextText: 'Next »',
+  align: true
+};
+
+const PAGER_TEMPLATE = `
+    <ul class="pagination">
+      <li class="arrow pagination-previous"
+          [class.unavailable]="noPrevious()"
+          [class.left]="!noPrevious()"
+          [ngClass]="{'pull-right': align}">
+        <a href (click)="selectPage(page - 1, $event)">{{getText('previous')}}</a>
+      </li>
+      <li class="arrow pagination-next"
+          [class.unavailable]="noNext()"
+          [class.right]="!noNext()"
+          [ngClass]="{'pull-right': align}">
+        <a href (click)="selectPage(page + 1, $event)">{{getText('next')}}</a>
+      </li>
+  </ul>
+`;
+
+/* tslint:disable */
+@Component({
+  selector: 'pager[ngModel]',
+  template: PAGER_TEMPLATE,
+  directives: [NgClass],
+  inputs: [
+    'align',
+    'totalItems', 'itemsPerPage',
+    'previousText', 'nextText',
+  ]
+})
+/* tslint:enable */
+
+export class PagerComponent extends PaginationComponent implements OnInit {
+  public config:any = pagerConfig;
+
+  public constructor(@Self() cd:NgModel, renderer:Renderer, elementRef:ElementRef) {
+    super(cd, renderer, elementRef);
+  }
+}
--- ng2-foundation.ts
+++ ng2-foundation.ts
@@ -1,13 +1,16 @@
-import { BUTTON_DIRECTIVES } from './components/buttons';
-import { AlertComponent } from './components/alert';
+import { AlertComponent }        from './components/alert';
+import { BUTTON_DIRECTIVES }     from './components/buttons';
+import { PAGINATION_DIRECTIVES } from './components/pagination';
 
-export * from './components/buttons';
 export * from './components/alert';
+export * from './components/buttons';
+export * from './components/pagination';
 
 export default {
   directives: [
     AlertComponent,
-    BUTTON_DIRECTIVES
+    BUTTON_DIRECTIVES,
+    PAGINATION_DIRECTIVES
   ],
   providers: []
 };
